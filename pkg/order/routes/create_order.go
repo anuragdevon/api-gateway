39cfbe23f0189e97329a7725db76411d035f92e6
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-23 12:59:51 +0530

    create: create order testcases and method

--- pkg/order/routes/create_order.go
+++ pkg/order/routes/create_order.go
@@ -1,1 +1,39 @@
 package routes
+
+import (
+	"context"
+	"net/http"
+
+	"api-gateway/pkg/order/pb"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateOrderRequestBody struct {
+	ItemId   int64 `json:"itemId"`
+	Quantity int64 `json:"quantity"`
+	UserId   int64 `json:"user_id"`
+}
+
+func CreateOrder(ctx *gin.Context, c pb.OrderServiceClient) {
+	body := CreateOrderRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.CreateOrder(context.Background(), &pb.CreateOrderRequest{
+
+		UserId:   body.UserId,
+		ItemId:   body.ItemId,
+		Quantity: body.Quantity,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
--- pkg/order/routes/create_order_test.go
+++ pkg/order/routes/create_order_test.go
@@ -1,1 +1,87 @@
 package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"api-gateway/pkg/order/pb"
+	"api-gateway/pkg/order/routes/mocks"
+)
+
+func TestCreateOrder(t *testing.T) {
+	router := gin.Default()
+
+	t.Run("CreateOrder method should return status 201 Created for a successful order creation", func(t *testing.T) {
+		mockClient := new(mocks.OrderServiceClient)
+
+		router.POST("/order", func(ctx *gin.Context) {
+			CreateOrder(ctx, mockClient)
+		})
+
+		requestBody := CreateOrderRequestBody{
+			UserId:   12,
+			ItemId:   123,
+			Quantity: 10,
+		}
+
+		jsonBody := `{"itemId": 123, "quantity": 10, "user_id": 12}`
+
+		req, err := http.NewRequest("POST", "/order", strings.NewReader(jsonBody))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+
+		recorder := httptest.NewRecorder()
+
+		expectedRequest := &pb.CreateOrderRequest{
+			UserId:   requestBody.UserId,
+			ItemId:   requestBody.ItemId,
+			Quantity: requestBody.Quantity,
+		}
+		expectedResponse := &pb.CreateOrderResponse{
+			Status: 201,
+		}
+		mockClient.On("CreateOrder", mock.Anything, expectedRequest).Return(expectedResponse, nil)
+
+		router.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusCreated, recorder.Code)
+
+		expectedResponseBody := `{"status":201}`
+		assert.Equal(t, expectedResponseBody, recorder.Body.String())
+
+		mockClient.AssertCalled(t, "CreateOrder", mock.Anything, expectedRequest)
+	})
+
+	t.Run("CreateOrder method should return status 400 BadRequest for invalid request", func(t *testing.T) {
+		mockClient := new(mocks.OrderServiceClient)
+
+		jsonBody := `{"itemId": "abc"}`
+		req, err := http.NewRequest("POST", "/orders", strings.NewReader(jsonBody))
+		assert.NoError(t, err)
+
+		req.Header.Set("Content-Type", "application/json")
+
+		recorder := httptest.NewRecorder()
+
+		var orderServiceClient mocks.OrderServiceClient = *mockClient
+
+		router.POST("/orders", func(ctx *gin.Context) {
+			CreateOrder(ctx, &orderServiceClient)
+		})
+
+		router.ServeHTTP(recorder, req)
+
+		mockClient.AssertNotCalled(t, "CreateOrder")
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+
+		expectedResponseBody := `{"error":"invalid request body"}`
+		assert.Equal(t, expectedResponseBody, recorder.Body.String())
+	})
+}
