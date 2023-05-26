371103bf4a94ae4481bc12144e1297e262361c84
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-26 08:01:02 +0530

    refactor: delete_item method and test

--- pkg/inventory/routes/delete_item_test.go
+++ pkg/inventory/routes/delete_item_test.go
@@ -1,76 +1,71 @@
 package routes
 
-// import (
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
-// 	"api-gateway/pkg/inventory/pb"
-// 	"api-gateway/pkg/inventory/routes/mocks"
-// )
+	"api-gateway/pkg/inventory/pb"
+	"api-gateway/pkg/inventory/pb/mocks"
+)
 
-// func TestDeleteItem(t *testing.T) {
-// 	router := gin.Default()
-// 	t.Run("DeleteItem method to return status 200 OK for successfully deletion of item", func(t *testing.T) {
-// 		mockClient := new(mocks.InventoryServiceClient)
+func TestDeleteItem(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
 
-// 		router.DELETE("/inventory/:id", func(ctx *gin.Context) {
-// 			DeleteItem(ctx, mockClient)
-// 		})
+	mockClient := new(mocks.InventoryServiceClient)
 
-// 		itemID := "123"
-// 		req, err := http.NewRequest("DELETE", "/inventory/"+itemID, nil)
-// 		assert.NoError(t, err)
+	var inventoryServiceClient pb.InventoryServiceClient = mockClient
+	router.DELETE("/inventory/:id", func(ctx *gin.Context) {
+		DeleteItem(ctx, inventoryServiceClient)
+	})
+	t.Run("DeleteItem method to return status 200 OK for successfully deletion of item", func(t *testing.T) {
+		itemID := "123"
+		req, err := http.NewRequest("DELETE", "/inventory/"+itemID, nil)
+		assert.NoError(t, err)
 
-// 		recorder := httptest.NewRecorder()
+		recorder := httptest.NewRecorder()
 
-// 		expectedRequest := &pb.DeleteItemRequest{
-// 			Id: 123,
-// 		}
-// 		expectedResponse := &pb.DeleteItemResponse{
-// 			Status: 200,
-// 		}
-// 		mockClient.On("DeleteItem", mock.Anything, expectedRequest).Return(expectedResponse, nil)
+		expectedRequest := &pb.DeleteItemRequest{
+			Id: 123,
+		}
+		expectedResponse := &pb.DeleteItemResponse{
+			Status: 200,
+		}
+		mockClient.On("DeleteItem", mock.Anything, expectedRequest).Return(expectedResponse, nil)
 
-// 		router.ServeHTTP(recorder, req)
+		router.ServeHTTP(recorder, req)
 
-// 		assert.Equal(t, http.StatusOK, recorder.Code)
+		assert.Equal(t, http.StatusOK, recorder.Code)
 
-// 		expectedResponseBody := `{"status":200}`
-// 		assert.Equal(t, expectedResponseBody, recorder.Body.String())
+		expectedResponseBody := `{"status":200}`
+		assert.Equal(t, expectedResponseBody, recorder.Body.String())
 
-// 		mockClient.AssertCalled(t, "DeleteItem", mock.Anything, expectedRequest)
-// 	})
+		mockClient.AssertCalled(t, "DeleteItem", mock.Anything, expectedRequest)
+	})
 
-// 	t.Run("DeleteItem method to return status 502 BadGateway for an error from the inventory service", func(t *testing.T) {
-// 		mockClient := new(mocks.InventoryServiceClient)
+	t.Run("DeleteItem method to return status 502 BadGateway for an error from the inventory service", func(t *testing.T) {
+		itemID := "456"
+		req, err := http.NewRequest("DELETE", "/inventory/"+itemID, nil)
+		assert.NoError(t, err)
 
-// 		itemID := "456"
-// 		req, err := http.NewRequest("DELETE", "/inventory/"+itemID, nil)
-// 		assert.NoError(t, err)
+		recorder := httptest.NewRecorder()
 
-// 		recorder := httptest.NewRecorder()
+		expectedRequest := &pb.DeleteItemRequest{
+			Id: 456,
+		}
+		mockClient.On("DeleteItem", mock.Anything, expectedRequest).Return(nil, errors.New("bad gateway error"))
 
-// 		var inventoryServiceClient mocks.InventoryServiceClient = *mockClient
+		router.ServeHTTP(recorder, req)
 
-// 		router.DELETE("/inventory/:id", func(ctx *gin.Context) {
-// 			DeleteItem(ctx, &inventoryServiceClient)
-// 		})
+		mockClient.AssertCalled(t, "DeleteItem", mock.Anything, expectedRequest)
 
-// 		expectedRequest := &pb.DeleteItemRequest{
-// 			Id: 456,
-// 		}
-// 		mockClient.On("DeleteItem", mock.Anything, expectedRequest).Return(nil, "some_internal_service_error")
+		assert.Equal(t, http.StatusBadGateway, recorder.Code)
 
-// 		router.ServeHTTP(recorder, req)
-
-// 		mockClient.AssertCalled(t, "GetItem", mock.Anything, expectedRequest)
-
-// 		assert.Equal(t, http.StatusBadGateway, recorder.Code)
-
-// 	})
-// }
+	})
+}
