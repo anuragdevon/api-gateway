f4f1894f2db0c5455a38eebdf4da1eb85eb3baec
anuragdevon <anuragkar@outlook.com>
2023-05-29 10:47:30 +0530

    refactor: change grpc transport dialup method

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -7,6 +7,7 @@ import (
 	"api-gateway/pkg/order/pb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ServiceClient struct {
@@ -14,7 +15,7 @@ type ServiceClient struct {
 }
 
 func InitServiceClient(c *config.Config) pb.OrderServiceClient {
-	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
+	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		fmt.Println("Could not connect:", err)
