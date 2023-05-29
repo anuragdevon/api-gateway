885bea2058baf1b986010b7fa5c91d5a26c1bcf6
anuragdevon <anuragkar@outlook.com>
2023-05-29 13:56:27 +0530

    refactor: dto update price type

--- pkg/inventory/routes/dto/inventory_dto.go
+++ pkg/inventory/routes/dto/inventory_dto.go
@@ -1,14 +1,14 @@
 package dto
 
 type CreateItemRequestBody struct {
-	Name     string `json:"name"`
-	Quantity int64  `json:"quantity"`
-	Price    int64  `json:"price"`
+	Name     string  `json:"name"`
+	Quantity int64   `json:"quantity"`
+	Price    float64 `json:"price"`
 }
 
 type UpdateItemRequestBody struct {
-	Id       int64  `json:"id"`
-	Name     string `json:"name"`
-	Quantity int64  `json:"quantity"`
-	Price    int64  `json:"price"`
+	Id       int64   `json:"id"`
+	Name     string  `json:"name"`
+	Quantity int64   `json:"quantity"`
+	Price    float64 `json:"price"`
 }
